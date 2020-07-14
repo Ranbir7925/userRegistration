46886d310fc9c824b0340c60c99cc5b22d40a5d6
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-07-14 20:14:32 +0530

    Last name validation done

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
-read -p "Enter Name: " name
+echo "Registration Form"
 
-namePattern="^[A-Z]{1}[a-z]{2,}"
 
-if [[ $name =~ $namePattern ]]
-then
-	echo "Valid"
-else
-	echo "Invalid"
-fi
+function registrationForm () {
+
+	read -p "Enter First Name: " Fname
+	read -p "Enter Last Name: " Lname
+	checker
+}
+
+function checker() {
+
+	FnamePattern="^[A-Z]{1}[a-z]{2,}"
+	LnamePattern="^[A-Z]{1}[a-z]{2,}"
+
+	if [[ $Fname =~ $FnamePattern ]]
+	then
+		echo "Valid"
+	else
+		echo "Invalid"
+	fi
+
+	if [[ $Lname =~ $LnamePattern ]]
+   then
+      echo "Valid"
+   else
+      echo "Invalid"
+   fi
+
+}
+registrationForm
