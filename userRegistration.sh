032c891771a1a7466c80156fb4bbef5806041a0d
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-07-14 21:33:17 +0530

    Password condition-1 is checked

--- userRegistration.sh
+++ userRegistration.sh
@@ -6,16 +6,18 @@ function registrationForm () {
 
 	read -p "Enter First Name: " Fname
 	read -p "Enter Last Name: " Lname
-	read -p "Enter Phone no: " phone
+	read -p "Enter Phone no: " Phone
+	read -p "Enter Password: " Password
 	checker
 }
 
 function checker() {
 
-	FnamePattern="^[A-Z]{1}[a-z]{2,}"
-	LnamePattern="^[A-Z]{1}[a-z]{2,}"
-	phonePattern="^[0-9]{1,3}[ ][1-9]{1}[0-9]{9}"
-	
+	FnamePattern="^[A-Z]{1}[a-z]{2,}$"
+	LnamePattern="^[A-Z]{1}[a-z]{2,}$"
+	PhonePattern="^[0-9]{1,3}[ ][1-9]{1}[0-9]{9}$"
+	PasswordPattern="^([a-zA-Z0-9@#!]){8,}$"
+
 	if [[ $Fname =~ $FnamePattern ]]
 	then
 		echo "Valid"
@@ -33,6 +35,13 @@ function checker() {
 	if [[ $phone =~ $phonePattern ]]
 	then
 		echo "valid"
+	else
+		echo "Invalid"
+	fi
+
+	if [[ $password =~ $passwordPattern ]]
+	then
+		echo "Valid"
 	else
 		echo "Invalid"
 	fi
