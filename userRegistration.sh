3e766ec267be537e920e60becb716fe7df1d8d7e
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-07-20 15:51:41 +0530

    Added password and email

--- userRegistration.sh
+++ userRegistration.sh
@@ -7,7 +7,8 @@ function registrationForm () {
 	read -p "Enter First Name: " Fname
 	read -p "Enter Last Name: " Lname
 	read -p "Enter Phone no: " Phone
-	read -p "Enter Password: " Password
+	read -p "Enter Password: " password
+	read -p "Enter Email: " Email
 	checker
 }
 
@@ -17,7 +18,9 @@ function checker() {
 	LnamePattern="^[A-Z]{1}[a-z]{2,}$"
 	PhonePattern="^[0-9]{1,3}[ ][1-9]{1}[0-9]{9}$"
 	PasswordPattern="^([a-zA-Z0-9@#!]){8,}$"
-
+	oneUpperCaseLetter="^[A-Za-z0-9]{0,1}([A-Z]+)?[a-zA-Z0-9]{8,}$"
+	atleatOneNumeric="^(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}$"
+	EmailPattern="^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$"
 	if [[ $Fname =~ $FnamePattern ]]
 	then
 		echo "Valid"
@@ -32,18 +35,39 @@ function checker() {
       echo "Invalid"
    fi
 
-	if [[ $phone =~ $phonePattern ]]
+	if [[ $Phone =~ $PhonePattern ]]
 	then
 		echo "valid"
 	else
 		echo "Invalid"
 	fi
 
-	if [[ $password =~ $passwordPattern ]]
+	if [[ $password =~ $PasswordPattern ]]
 	then
 		echo "Valid"
 	else
 		echo "Invalid"
 	fi
+	
+	if [[ $password =~ $oneUpperCaseLetter ]]
+	then
+		echo $password Successfully registered one UpperCase
+	else
+		echo Invalid Uppercase
+	fi
+	
+	if [[ $password =~ $atleatOneNumeric ]]
+	then
+		echo $password Successfully registered one numeric
+	else
+		echo Invalid One Numeric
+	fi
+
+	if [[ $Email =~ $EmailPattern ]]
+   then
+      echo "Valid"
+   else
+      echo "Invalid"
+   fi
 }
 registrationForm
